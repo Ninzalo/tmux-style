8ef191991cb642ee1656439bb0038e1b9e52e7a2
Ninzalo <k-artemiy@mail.ru>
2024-10-19 02:44:03 +0300

    feat(utils/parse_widgets): update script for dynamic params amount

--- utils/parse_widgets.sh
+++ utils/parse_widgets.sh
@@ -27,18 +27,24 @@ if [ -z "$WIDGETS" ]; then
 fi
 
 for WIDGET in $WIDGETS; do
-  WIDGET_NAME=$(echo "$WIDGET" | sed -n "s/^@$PLUGIN_NAME-\(.*\)-widget$/\1/p")
+  WIDGET_NAME=$(echo "$WIDGET" | sed 's/-[0-9]\+-widget$/-widget/' | sed -n "s/^@$PLUGIN_NAME-\(.*\)-widget$/\1/p")
+  PARAMS_AMOUNT=$(echo "$WIDGET" | sed -n "s/^@$PLUGIN_NAME-.*-\([0-9]\+\)-widget$/\1/p")
+
+  if [ -z "$PARAMS_AMOUNT" ]; then
+    PARAMS_AMOUNT=4
+  fi
 
   if [ -n "$WIDGET_NAME" ]; then
-    echo "Found widget: $WIDGET_NAME"
-    sh "$CURRENT_PATH/utils/set_widget_options.sh" "$PLUGIN_NAME" "$WIDGET_NAME"
+    echo "Found widget: $WIDGET_NAME with params amount: $PARAMS_AMOUNT"
+    sh "$CURRENT_PATH/utils/set_widget_options.sh" "$PLUGIN_NAME" "$WIDGET_NAME" "$PARAMS_AMOUNT"
     
     if [ $? -ne 0 ]; then
       echo "Error: Failed to set widget options for $WIDGET_NAME"
       exit 1
     fi
-  else
+  fi
+
+  if [ -z "$WIDGET_NAME" ]; then
     echo "Warning: Failed to extract widget name from $WIDGET"
   fi
 done
-
