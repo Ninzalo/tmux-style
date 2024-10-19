e65624808d67a51ed54e54af6a29f306e1192f9e
Ninzalo <k-artemiy@mail.ru>
2024-10-19 12:01:55 +0300

    refactor(utils/parse_widgets): refactor loop

--- utils/parse_widgets.sh
+++ utils/parse_widgets.sh
@@ -28,23 +28,23 @@ fi
 
 for WIDGET in $WIDGETS; do
   WIDGET_NAME=$(echo "$WIDGET" | sed 's/-[0-9]\+-widget$/-widget/' | sed -n "s/^@$PLUGIN_NAME-\(.*\)-widget$/\1/p" | sed 's/-*$//')
+
+  if [ -z "$WIDGET_NAME" ]; then
+    echo "Warning: Failed to extract widget name from $WIDGET"
+    continue
+  fi
+
   PARAMS_AMOUNT=$(echo "$WIDGET" | sed -n "s/^@$PLUGIN_NAME-.*-\([0-9]\+\)-widget$/\1/p")
 
   if [ -z "$PARAMS_AMOUNT" ]; then
     PARAMS_AMOUNT=4
   fi
 
-  if [ -n "$WIDGET_NAME" ]; then
-    echo "Found widget: $WIDGET_NAME with params amount: $PARAMS_AMOUNT"
-    sh "$CURRENT_PATH/utils/set_widget_options.sh" "$PLUGIN_NAME" "$WIDGET_NAME" "$PARAMS_AMOUNT"
-    
-    if [ $? -ne 0 ]; then
-      echo "Error: Failed to set widget options for $WIDGET_NAME"
-      exit 1
-    fi
-  fi
-
-  if [ -z "$WIDGET_NAME" ]; then
-    echo "Warning: Failed to extract widget name from $WIDGET"
+  echo "Found widget: $WIDGET_NAME with params amount: $PARAMS_AMOUNT"
+  sh "$CURRENT_PATH/utils/set_widget_options.sh" "$PLUGIN_NAME" "$WIDGET_NAME" "$PARAMS_AMOUNT"
+  
+  if [ $? -ne 0 ]; then
+    echo "Error: Failed to set widget options for $WIDGET_NAME"
+    exit 1
   fi
 done
