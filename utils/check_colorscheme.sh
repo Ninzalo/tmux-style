e561d3770b1b6a4157094f1619e99b894cb6b893
Ninzalo <k-artemiy@mail.ru>
2024-10-23 18:57:15 +0300

    feat(utils/check_colorscheme): implement colorscheme validation and fallback logic
    - Added initial version of check_colorscheme.sh script.
    - Validates user-defined custom colorscheme via
    @${PLUGIN_NAME}-custom-colorscheme-path.
    - Checks for default colorschemes in themes directory.
    - Displays error messages in tmux status-line if colorscheme is not
    found.
    - Falls back to 'gruvbox-dark' if no valid colorscheme is available.

---
+++ utils/check_colorscheme.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+PLUGIN_NAME=$1
+CURRENT_FILE=$2
+COLORSCHEME=$3
+
+CUSTOM_COLORSCHEME_PATH=$(tmux show-option -gqv @${PLUGIN_NAME}-custom-colorscheme-path)
+
+if [[ -n "$CUSTOM_COLORSCHEME_PATH" ]]; then
+    if [[ -f "$CUSTOM_COLORSCHEME_PATH" ]]; then
+        tmux set-option -g @${PLUGIN_NAME}-colorscheme "$CUSTOM_COLORSCHEME_PATH"
+        exit 0
+    else
+        tmux display-message "Custom colorscheme file not found: $CUSTOM_COLORSCHEME_PATH"
+    fi
+fi
+
+if [[ -f "${CURRENT_FILE}/themes/${COLORSCHEME}.conf" ]]; then
+    tmux set-option -g @${PLUGIN_NAME}-colorscheme "$CURRENT_FILE/themes/${COLORSCHEME}.conf"
+    exit 0
+else
+    tmux display-message "Failed to set ${COLORSCHEME} as a colorscheme. Defaulting to Gruvbox-dark"
+    tmux set-option -g @${PLUGIN_NAME}-colorscheme "$CURRENT_FILE/themes/gruvbox-dark.conf"
+fi
