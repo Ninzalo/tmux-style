cdd056bdb571ce94ee39fd2a0b6b2c1166d5f928
Ninzalo <k-artemiy@mail.ru>
2024-10-23 19:54:05 +0300

    refactor(utils/check_colorscheme): simplify colorscheme loading
    - Removed the need to pass COLORSCHEME as a parameter to the
    check_colorscheme.sh script.

--- utils/check_colorscheme.sh
+++ utils/check_colorscheme.sh
@@ -2,8 +2,8 @@
 
 PLUGIN_NAME=$1
 CURRENT_FILE=$2
-COLORSCHEME=$3
 
+COLORSCHEME=$(tmux show-option -gqv @${PLUGIN_NAME}-colorscheme)
 CUSTOM_COLORSCHEME_PATH=$(tmux show-option -gqv @${PLUGIN_NAME}-custom-colorscheme-path)
 
 if [[ -n "$CUSTOM_COLORSCHEME_PATH" ]]; then
@@ -19,6 +19,6 @@ if [[ -f "${CURRENT_FILE}/themes/${COLORSCHEME}.conf" ]]; then
     tmux set-option -g @${PLUGIN_NAME}-colorscheme "$CURRENT_FILE/themes/${COLORSCHEME}.conf"
     exit 0
 else
-    tmux display-message "Failed to set ${COLORSCHEME} as a colorscheme. Defaulting to Gruvbox-dark"
+    tmux display-message "Failed to set [${COLORSCHEME}] as a colorscheme. Defaulting to Gruvbox-dark"
     tmux set-option -g @${PLUGIN_NAME}-colorscheme "$CURRENT_FILE/themes/gruvbox-dark.conf"
 fi
